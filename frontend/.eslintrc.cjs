e49d75e1baa3849f06f21a3f788b6ea2b99a2655
Coralie Acquaert <coralie.acquaert@gmail.com>
2023-05-08 13:37:23 +0200

    (frontend): configure eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,12 +3,20 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  },
+  env: {
+    node: true,
+    browser: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  ignorePatterns: ['*.html']
 }
