616910452ac53a1a29d029e2c17106593f954bdd
Coralie Acquaert <coralie.acquaert@gmail.com>
2023-05-08 20:53:54 +0200

    (frontend) feat[random-button]: add loader with delay for generate message

--- frontend/src/components/RandomButton.vue
+++ frontend/src/components/RandomButton.vue
@@ -1,6 +1,7 @@
 <script setup>
 import useExcusesStore from '../stores/excuses.store'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import ButtonLoader from './ButtonLoader.vue'
 
 const props = defineProps({
   messageExcuse: String
@@ -15,12 +16,18 @@ computed({
   set: (value) => emit('random-message', value)
 })
 
+const isLoading = ref(false)
+
 const NewRamdomExcuse = () => {
+  isLoading.value = true
   let newRandomMessage = excusesStore.getRandomExcuse().message
   if (props.messageExcuse === newRandomMessage) {
     NewRamdomExcuse()
   } else {
-    emit('random-message', newRandomMessage)
+    setTimeout(() => {
+      emit('random-message', newRandomMessage)
+      isLoading.value = false
+    }, Math.floor(Math.random() * 5000) + 1000)
   }
 }
 </script>
@@ -28,9 +35,11 @@ const NewRamdomExcuse = () => {
 <template>
   <button
     type="button"
+    :disabled="isLoading"
     @click.prevent="NewRamdomExcuse"
-    class="px-5 m-auto w-1/2 max-w-xs inline py-3 text-l md:text-xl leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-green-500 hover:bg-green-500"
+    class="px-5 m-auto w-1/2 max-w-xs flex justify-center items-center inline py-3 text-l md:text-xl leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-green-500 hover:bg-green-500"
   >
+    <ButtonLoader v-if="isLoading" />
     <span>Random excuse</span>
   </button>
 </template>
