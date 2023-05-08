1d56276be1cfa9fe65398f90ac6b020adb2bbc7c
Coralie Acquaert <coralie.acquaert@gmail.com>
2023-05-08 12:12:18 +0200

    (backend) test: post method return 201

--- backend/__tests__/excuses.spec.js
+++ backend/__tests__/excuses.spec.js
@@ -19,3 +19,16 @@ test('#1 GET / - Without data return 202', async () => {
   expect(res.statusCode).toBe(202)
   expect(res.body.message).toBe('no excuses available')
 })
+
+test('#2 POST / - With valid data return 201', async () => {
+  const res = await request(app).post('/api/excuse').send({
+    http_code: 500,
+    tag: 'test',
+    message: 'test'
+  })
+  expect(res).toBeDefined()
+  expect(res.statusCode).toBe(201)
+  expect(res.body.http_code).toBe(500)
+  expect(res.body.tag).toBe('test')
+  expect(res.body.message).toBe('test')
+})
