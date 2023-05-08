4350940aecf3d52195b2ca8864d03ffc49c5063d
Coralie Acquaert <coralie.acquaert@gmail.com>
2023-05-08 16:29:32 +0200

    (frontend) feat[404]: redirect to the home page with router link

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -9,10 +9,10 @@
           Sorry we couldn't find the page you're looking for
         </p>
 
-        <a
-          href="#"
+        <router-link
+          :to="{ name: 'DevExcuses' }"
           class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700"
-          >back to homepage</a
+          >back to homepage</router-link
         >
       </div>
       <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
